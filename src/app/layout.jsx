a4f4022de2aeea5f00f3e4d3c9499e71c57f3938
SedPC <lilshed3@gmail.com>
2024-04-22 15:04:10 -0700

    working on responsiveness of mobile vs. desktop

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,16 +4,28 @@ import { Inter } from 'next/font/google';
 import './(styles)/globals.css';
 import Navbar from './(components)/Navbar.jsx';
 import { Analytics } from "@vercel/analytics/react";
+import { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  //check if is mobile/desktop upon render
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 720);
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        {
+          //return mobile components if queryDeviceType returns true
+          //else return desktop components
+          isMobile ?  <h1>Hello Mobile</h1> : <h1>Hello Desktop</h1>
+        }
         {children}
-        <Analytics />
       </body>
     </html>
   )
